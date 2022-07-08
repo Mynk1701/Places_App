2a879748ce20fd4387752bed773c5f1fd77209aa
Mynk1701 <108948192+Mynk1701@users.noreply.github.com>
2022-07-09 02:59:12 +0530

    Add files via upload

---
+++ Places/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views 
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include("App.urls")),
+    path('register', views.Register),
+    path('login', views.Login),
+    path('logout', views.Logout),
+    path('accounts/', include('allauth.urls')),
+]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
